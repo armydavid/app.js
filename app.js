cd5f1ed59c097823847bc0f1c29f1cbb1a37b1d5
armydavid <65372984+armydavid@users.noreply.github.com>
2020-08-28 23:19:31 -0400

    commit changes

---
+++ app.js
@@ -0,0 +1,77 @@
+// Write code to create a file called app.js
+
+//console.log('Hello Node!')
+//var message = 'Hello Node!';
+
+//var sum = 5 + 3;
+//console.log(message);
+//console.log(sum);
+
+//var commandLineArgs = process.argv;
+//console.log(commandLineArgs);
+
+//var profileDataArgs = process.argv.slice(2,process.argv.length);
+//console.log(profileDataArgs);
+
+//var profileDataArgs = process.argv.slice(2,process.argv.length -1);
+//console.log(profileDataArgs);
+
+//const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+//console.log(animals.slice(2));
+
+//const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+//console.log(animals.slice(2, 4));
+
+//const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+//console.log(animals.slice(2, 5));
+
+//This errors out
+//const message = 'Hello Node!';
+//message = 'Goodby Node!';
+//const sum = 5 +3;
+//sum += 1;
+
+//const animalArray = ['dog', 'cat', 'pig'];
+//animalArray.push('cow');
+//const personObj = { name: 'Lernantino', age: 99 };
+//personObj.age = 100;
+//personObj.occupation = 'Developer';
+
+//This errors out
+//const number = 42
+//try {
+ // number = 99;
+//} catch (err)
+//{
+//console.log(err)
+//}
+
+//const number = 42
+//console.log(number)
+
+//const printProfileData = (profileDataArr) => {
+//  console.log(profileDataArr);
+//};
+//var profileDataArgs = 'hello';
+//printProfileData(profileDataArgs);
+
+//Using function expression syntax
+//const addNums = function(numOne, numTwo) {
+ // return numOne + numTwo;
+//};
+
+//Using new arrow function syntax
+//const addNums = (numOne, numTwo) => {
+  //return numOne + numTwo;
+//};
+
+//const addNums = (numOne, numTwo) => {
+  //console.log(numOne, numTwo);
+  //return numOne + numTwo;
+//};
+
+let message = 'Hello Node!';
+message = 'Hello ES6';
+
+let sum = 5 + 3;
+sum += 1;
